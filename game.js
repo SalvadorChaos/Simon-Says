6b4fea4351fc7bdca2e8a8864a6a23d95cc46a5a
Salvador Martinez <47091900+SalvadorChaos@users.noreply.github.com>
2019-09-07 20:05:50 -0500

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keydown(function() {
+$(document).on('click touch', function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
